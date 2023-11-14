17eb7a98feca4a24a57b6f7d89b223757d8ee898
Sergei <legal.666@mail.ru>
2023-11-14 14:29:37 +0300

    Software_architecture_7

---
+++ js/router.js
@@ -0,0 +1,31 @@
+
+
+
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+
+const handleLocation = async() => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+};
+
+const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/home.html",
+    "/about": "/pages/about.html",
+    "/contact": "/pages/contact.html",
+    "/services": "/pages/services.html", // Добавленный путь
+    "/portfolio": "/pages/portfolio.html", // Добавленный путь
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
